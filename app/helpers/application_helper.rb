e36b8205df0b564f9117732fa9cd63b861621a9b
chrsearl <chris.searle@finn.no>
2014-07-09 09:04:04 +0200

    Bootstrapping

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def bootstrap_type(type)
+    case type
+      when :alert
+        'warning'
+      when :error
+        'danger'
+      when :notice
+        'info'
+      when :success
+        'success'
+      else
+        type.to_s
+    end
+  end
 end
